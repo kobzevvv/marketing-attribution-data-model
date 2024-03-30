3ee770b8807dc2238af602482526d31ef5c4c508
VLADIMIR KOBZEV <vladimir.kobzev@improvado.io>
2024-03-29 22:10:40 -0300

    event stream contract

---
+++ data_models/1_data_source_preparation/google_analytics_4/ga4_events_extract_features_from_event_params.sql
@@ -0,0 +1,50 @@
+with 
+
+
+    filtered_event_params_cte as (
+        select
+
+        event_name,
+
+        ARRAY(
+            select  
+                event_param.value.string_value                                          as string_value
+
+            from UNNEST(event_params) as event_param
+
+            where 
+                event_param.key = 'hashed_email'
+            and 
+                event_param.value.string_value !='null'
+
+        )[SAFE_OFFSET(0)]                                                               as hashed_email,
+
+        ARRAY(
+            select  
+            as struct 
+                ep.key, 
+                ep.value.string_value                                                   as string_value
+
+            from UNNEST(event_params) as ep
+
+            where 
+                ep.key IN (
+                'source',
+                'medium', 
+                'campaign',
+                'term',
+                'page_location'
+                ) 
+            AND 
+                ep.value.string_value !='null'
+        )                                                                               as filtered_event_params,
+
+        *
+
+    from `improvado-9598-90e.analytics_407788275.events_20*`
+  )
+
+select *
+from filtered_event_params_cte
+where 
+  ARRAY_LENGTH(filtered_event_params) > 0
